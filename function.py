79c32c3fc17d654a5a51b3dfd7e611012ab2c285
nitty fritty <nathan.fredette@libertymutual.com>
2026-04-09 20:10:59 +0000

    add conditions to function

--- function.py
+++ function.py
@@ -5,8 +5,16 @@ def get_user_name() -> str:
         str: Greeting message with the user's name.
 
     """
-    name = str(input("What is your name? "))
-        
+    initial_input = input("What is your name? ")
+
+    if initial_input == "" or initial_input.isspace():
+        print("Please enter a valid name.")
+        return get_user_name()
+    elif initial_input.isdigit():
+        print("Please enter a string value for your name, not just numbers.")
+        return get_user_name()
+    else:
+        name = initial_input.strip()
     printname = f"Hello {name}! Have a great day."
     return printname
 
