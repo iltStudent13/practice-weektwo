9acb72d42b46c18cbd97536565d7b4dd1240a8f1
nitty fritty <nathan.fredette@libertymutual.com>
2026-04-09 19:57:05 +0000

    alter function in hellowworld

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
-print("hello world")
+def get_user_name() -> str:
+    """This function will ask the user for their name and return it as a string.
+    
+    Returns:
+        str: Greeting message with the user's name.
+
+    """
+    name = str(input("What is your name? "))
+        
+    printname = f"Hello {name}! Have a great day."
+    return printname
+
+if __name__ == "__main__":
+    print(get_user_name())
